d9d8874b05d276f6d336ea255f6398dabed62381
Adam Greig <random@randomskk.net>
2009-05-17 22:18:27 +0100

    Turn off at night time.

--- clock.c
+++ clock.c
@@ -32,6 +32,20 @@ unsigned short int sec_t, sec_u, min_t, min_u, hour_t, hour_u, day;
 unsigned short int date_t, date_u, month_t, month_u, year_t, year_u;
 
 //============================================================================
+// Power Supply Control
+//----------------------------------------------
+
+// Turn the power supply on (ground SHDN)
+void power_on() {
+    PORTC &= ~(1<<SHDN);
+}
+
+// Turn the power supply off (raise SHDN)
+void power_off() {
+    PORTC |= (1<<SHDN);
+}
+
+//============================================================================
 //============================================================================
 // Initialisation
 //----------------------------------------------
@@ -57,9 +71,20 @@ void setup() {
 //----------------------------------------------
 
 void loop() {
-    input_scan();
+    // Get the current time from the RTC
     rtc_read();
-    shift_out_time();
+    
+    // Check if it's too early in the morning to be showing the time
+    if((10*hour_t + hour_u) >= 7) {
+        // If so, power on the PSU, check the input buttons, then show the time
+        power_on();
+        input_scan();
+        shift_out_time();
+    } else {
+        // Or if not, turn off the PSU, disabling the tubes
+        power_off();
+    }
+    
     _delay_ms(400);
 }
 
